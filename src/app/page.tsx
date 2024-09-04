e1e76a7aaa0fc18446d3854da37afcdab5a5a90c
maalpi <mateus.mateus30@gmail.com>
2024-09-04 20:07:38 -0300

    navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,50 +12,33 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 import DarkLightToggle from "@/components/Theme";
+import { Navigation } from "@/components/Nav";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <DarkLightToggle />
-      <Card className="mx-auto max-w-sm">
+    <main className="flex min-h-screen items-center justify-between p-24">
+      <Navigation />
+      <Card className="mx-auto max-w-lg">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Entre com seu user
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
             <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
+              <Label htmlFor="email">user</Label>
               <Input
-                id="email"
-                type="email"
-                placeholder="m@example.com"
+                id="user"
+                type="user"
+                placeholder="user"
                 required
               />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link href="#" className="ml-auto inline-block text-sm underline">
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input id="password" type="password" required />
             </div>
             <Button type="submit" className="w-full">
               Login
             </Button>
-            <Button variant="outline" className="w-full">
-              Login with Google
-            </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            Don&apos;t have an account?{" "}
-            <Link href="#" className="underline">
-              Sign up
-            </Link>
           </div>
         </CardContent>
     </Card>
---
+++ src/components/Nav.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, PropsWithChildren, useState } from "react";
+
+import DarkLightToggle from "./Theme";
+export const Navigation: React.FC<PropsWithChildren<{ user: string}>> = ({user}) => {
+
+    return (
+        <header>
+            <div
+				className={`fixed inset-x-0 top-0 z-50 backdrop-blur  duration-200  bg-zinc-900/500 `}
+			>
+				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
+					<div className="flex justify-between gap-8">
+							<DarkLightToggle/>
+					</div>
+
+					<div
+						className="duration-200 text-zinc-300 hover:text-zinc-100"
+					>
+						<h1 className="text-xl font-bold">PPT Classificador</h1>
+					</div>
+				</div>
+			</div>
+
+        
+        </header>
+    );
+};
